8faeb0b295cfb6f4347d5152b20378bee9d7184e
Mikołaj Szydłowski <9szydlowski9@gmail.com>
2023-01-04 18:12:20 +0100

    feat: basic board logic (#3)
    * Added basic board logic and an example of it
    
    * Code review changes

---
+++ Warships/Board.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Media;
+
+namespace Warships;
+
+public class Board
+{
+    public int Height { get; set; }
+    public int Width { get; set; }
+
+    private EBoardRect[,] _shipsBoard;
+    private EBoardRect[,] _drawnBoard;
+
+    public Board(int height, int width)
+    {
+        Height = height;
+        Width = width;
+        SetUpBoard();
+    }
+
+    private void SetUpBoard()
+    {
+        _shipsBoard = new EBoardRect[Height, Width];
+        _drawnBoard = new EBoardRect[Height, Width];
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                _shipsBoard[i, j] = EBoardRect.Empty;
+                _drawnBoard[i, j] = EBoardRect.Empty;
+            }
+        }
+    }
+
+    public EBoardRect RectAt(int y, int x)
+    {
+        return _shipsBoard[y, x];
+    }
+
+    public void SetRectAt(int y, int x, EBoardRect type)
+    {
+        _shipsBoard[y, x] = type;
+    }
+    
+    public EBoardRect DrawnRectAt(int y, int x)
+    {
+        return _drawnBoard[y, x];
+    }
+
+    public void SetDrawnRectAt(int y, int x, EBoardRect type)
+    {
+        _drawnBoard[y, x] = type;
+    }
+
+    public bool CanPlaceAt(int y, int x)
+    {
+        return _shipsBoard[y, x] != EBoardRect.Ship;
+    }
+}
---
+++ Warships/EBoardRect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Warships;
+
+public enum EBoardRect
+{
+    Hidden,
+    PlacementReady,
+    PlacementError,
+    Empty,
+    Ship,
+    Hit
+}
+
+public static class EBoardRectExtensions
+{
+    public static SolidColorBrush RectColor(this EBoardRect rect)
+    {
+        return rect switch
+        {
+            EBoardRect.Hidden => Brushes.DarkGray,
+            EBoardRect.PlacementReady => Brushes.Green,
+            EBoardRect.PlacementError => Brushes.Firebrick,
+            EBoardRect.Empty => Brushes.DarkSlateGray,
+            EBoardRect.Ship => Brushes.Black,
+            EBoardRect.Hit => Brushes.Yellow,
+            _ => Brushes.Gray
+        };
+    }
+}
