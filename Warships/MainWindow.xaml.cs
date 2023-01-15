77246ea7d288727a0e6451d076be25e7d859f8fc
Filip Szymeczko <kijolakijek3@gmail.com>
2023-01-15 20:59:17 +0100

    feat: Overlay, player 2 board

--- Warships/MainWindow.xaml.cs
+++ Warships/MainWindow.xaml.cs
@@ -22,25 +22,48 @@ namespace Warships
     public partial class MainWindow : Window
     {
         private Board _boardPlayer1;
-        private Board _boardPlayer2; // For future players
+        private Board _boardPlayer2;
         private const int RectangleSize = 80;
         
         public MainWindow()
         {
             InitializeComponent();
-            CreateBoard();
+            GameManager.GetInstance().GameStateChanged += OnGameStateChanged;
+            _boardPlayer1 = CreateBoard();
+            GameManager.GetInstance().GameState = EGameState.SecondPlayerBoardGeneration;
+            _boardPlayer2 = CreateBoard();
+            GameManager.GetInstance().GameState = EGameState.FirstPlayerPlacingShips;
         }
 
-        private void CreateBoard()
+        private Board CreateBoard()
         {
             var height = 10;
             var width = 10;
-            _boardPlayer1 = new Board(height, width);
-            RedrawBoard(_boardPlayer1);
+            var board = new Board(height, width);
+            RedrawBoard(board);
+            return board;
         }
-            
+
+        private void OnGameStateChanged()
+        {
+            if (GameManager.GetInstance().GameState == EGameState.SecondPlayerPlacingNext)
+            {
+                OverlayText.Text = "Player2 turn";
+                Player1.Visibility = Visibility.Hidden;
+                Player2.Visibility = Visibility.Hidden;
+                Overlay.Visibility = Visibility.Visible;
+            } else if (GameManager.GetInstance().GameState == EGameState.FirstPlayerTurnNext)
+            {
+                OverlayText.Text = "Player1 turn";
+                Player1.Visibility = Visibility.Hidden;
+                Player2.Visibility = Visibility.Hidden;
+                Overlay.Visibility = Visibility.Visible;
+            }
+        }
+        
         private void RedrawBoard(Board board)
         {
+            var canvasBoard = GameManager.GetInstance().GameState.IsFirstPlayer() ? Player1 : Player2;
             for (int i = 0; i < board.RedrawBuffer.Count; i++)
             {
                 var y = board.RedrawBuffer[i].Item1;
@@ -54,18 +77,29 @@ namespace Warships
                 Canvas.SetTop(boardRectangle.Rectangle, y * RectangleSize);
                 Canvas.SetLeft(boardRectangle.Rectangle, x * RectangleSize);
 
-                foreach (UIElement rect in Canvas.Children)
+                foreach (UIElement rect in canvasBoard.Children)
                 {
                     if (rect.Uid == id.ToString())
                     {
-                        Canvas.Children.Remove(rect);
+                        canvasBoard.Children.Remove(rect);
                         break;
                     }
                 }
                 
-                Canvas.Children.Add(boardRectangle.Rectangle);
+                canvasBoard.Children.Add(boardRectangle.Rectangle);
             }
             board.RedrawBuffer.Clear();
+        }
+
+        private void Overlay_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (GameManager.GetInstance().GameState == EGameState.SecondPlayerPlacingNext)
+            {
+                GameManager.GetInstance().GameState = EGameState.SecondPlayerPlacingShips;
+                Player1.Visibility = Visibility.Hidden;
+                Player2.Visibility = Visibility.Visible;
+                Overlay.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
