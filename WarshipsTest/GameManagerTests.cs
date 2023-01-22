e8e68b3a2a98c02f0785c3be7ccb8638f05e3185
Mikołaj Szydłowski <9szydlowski9@gmail.com>
2023-01-22 02:17:16 +0100

    Added Game Manager and Ships tests

---
+++ WarshipsTest/GameManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using Warships;
+
+namespace WarshipsTest;
+
+[TestFixture]
+public class GameManagerTests
+{
+    private bool _didInvoke = false;
+
+    private void OnGameStateChanged()
+    {
+        _didInvoke = true;
+    }
+    
+    [Test]
+    public void GameManagerInit()
+    {
+        Assert.Multiple(
+            () =>
+            {
+                Assert.That(GameManager.GetInstance(), Is.Not.Null);
+                Assert.That(GameManager.GetInstance().GameState, Is.EqualTo(EGameState.FirstPlayerBoardGeneration));
+                Assert.That(GameManager.GetInstance().IsBeginningTurns, Is.True);
+            }
+        );
+    }
+    
+    [Test]
+    [TestCase(EGameState.PlayerWaiting)]
+    [TestCase(EGameState.FirstPlayerTurn)]
+    [TestCase(EGameState.SecondPlayerPlacingShips)]
+    [TestCase(EGameState.FirstPlayerTurnNext)]
+    [TestCase(EGameState.SecondPlayerBoardGeneration)]
+    public void GameStateChangeInvoke(EGameState newState)
+    {
+        _didInvoke = false;
+        GameManager.GetInstance().GameStateChanged += OnGameStateChanged;
+        GameManager.GetInstance().GameState = newState;
+        Assert.That(_didInvoke, Is.True);
+        GameManager.GetInstance().GameStateChanged -= OnGameStateChanged;
+    }
+}
---
+++ WarshipsTest/ShipTests.cs
@@ -0,0 +1,62 @@
+﻿using Warships;
+using Moq;
+
+namespace WarshipsTest;
+
+[TestFixture]
+public class ShipTests
+{
+    private Ship _currentShip;
+    
+    [Test]
+    [TestCase(EShipType.Quadruple)]
+    [TestCase(EShipType.Triple)]
+    [TestCase(EShipType.Double)]
+    [TestCase(EShipType.Single)]
+    public void ShipInit(EShipType type)
+    {
+        _currentShip = new Ship(type);
+        Assert.Multiple( () => 
+        {
+            Assert.That(_currentShip.Type, Is.EqualTo(type));
+            Assert.That(_currentShip.Orientation, Is.EqualTo(EShipOrientation.Vertical));
+            Assert.That(_currentShip.IsSunk, Is.False);
+            Assert.That(_currentShip.StartPosition, Is.Null);
+        });
+    }
+
+    [Test]
+    public void ShipTiles([Values]EShipType type, [Values]EShipOrientation orientation, [Values(0,2)]int y, [Values(0,2)]int x)
+    {
+        _currentShip = new Ship(type)
+        {
+            Orientation = orientation,
+            StartPosition = Tuple.Create(y, x)
+        };
+
+        var properties = new BoardProperties(10, 10);
+        
+        var board = new Mock<Board>(10, 10);
+        board.SetupGet(b => b.BoardProperties).Returns(properties);
+
+        var resultList = new List<Tuple<int, int>>();
+        for (int i = 0; i < type.Length(); i++)
+        {
+            if (orientation == EShipOrientation.Vertical)
+            {
+                resultList.Add(Tuple.Create(y+i, x));
+            }
+            else
+            {
+                resultList.Add(Tuple.Create(y, x+i));
+            }
+        }
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(_currentShip.ShipTiles(Tuple.Create(y, x), board.Object), Is.EqualTo(resultList));
+            Assert.That(_currentShip.FinalShipTiles(), Is.Not.Null);
+            Assert.That(_currentShip.ShipTiles(Tuple.Create(y, x), board.Object), Is.EqualTo(_currentShip.FinalShipTiles()!));
+        });
+    }
+}
